1a1548eaf8d18fe786a1dfdedbbb85663f8a28a0
Sayain-Aditya <as4316664@gmail.com>
2026-06-09 11:47:02 +0530

    dsa

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
